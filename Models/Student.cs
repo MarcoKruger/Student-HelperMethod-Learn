547470a67d68fe64512ae8cde302c6760ed827a5
MarcoKruger <2908@ctucareer.co.za>
2018-09-12 12:40:54 +0200

    Initial commit

---
+++ Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Required]
+        [Display(Name = "Student Name")]
+        public string StudentName { get; set; }
+
+        [Required]
+        [Display(Name = "Student ID Number")]
+        public int StudentIdNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Student Email")]
+        public string StudentEmail { get; set; }
+
+        [Required]
+        [Display(Name = "Student Gender")]
+        public StudentAge StudentGender { get; set; }
+
+
+    }
+    public enum StudentAge
+    {
+        Male,
+        Female
+    }
+}
